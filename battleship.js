74e95db3f7bd7dc461ae109243e44f66a5e3f37d
Ray Jiang <CtD8864@gmail.com>
2021-09-07 14:52:47 +0800

    This if else statement tells whether player hits or not and gives responding alert.

--- battleship.js
+++ battleship.js
@@ -15,4 +15,15 @@ while (isSunk == false) {
     } else {
         guesses = guesses + 1;
     }
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+        alert("Hit !!");
+        hits = hits + 1;
+        if (hits == 3) {
+            isSunk = true;
+            alert("You've sunk my battleship!")
+        }
+    } else {
+        alert("You've missed!!")
+    }
 }
